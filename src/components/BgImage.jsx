d485094295713902acad2e439cc6f4040230bee0
MoriCiao <bboynoice@gmail.com>
2025-09-27 12:19:01 +0800

    refactor: 將TYype 抽取重構優化、BgImage元件優化

--- src/components/BgImage.jsx
+++ src/components/BgImage.jsx
@@ -1,10 +1,15 @@
+const STYLE = {
+  bg: `dataForm-bg pointer-events-none fixed top-0 left-0 z-[-2] h-full w-full overflow-hidden opacity-25 select-none`,
+};
+
 const BgImage = () => {
   return (
-    <section className="dataForm-bg pointer-events-none fixed top-0 left-0 z-[-2] h-full w-full overflow-hidden opacity-25 select-none">
+    <section className={STYLE.bg}>
       <img
         className="h-full w-full object-cover"
         src="/project-DataForm/bg_img.webp"
-        alt="BgImage"
+        alt="背景圖片"
+        loading="lazy"
       />
     </section>
   );
--- src/features/dataFormSlice.ts
+++ src/features/dataFormSlice.ts
@@ -1,83 +1,9 @@
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-
+import {Product, Products, status, DataFormState} from "./dataFormSliceType" 
 // 延遲載入
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 const url = "/project-DataForm/product_data_2000.json";
-// API 資料仔入
-
-type Product = {
-  id: string;
-  name: string;
-  category: "" | "居家生活" | "文具用品" | "電子產品" | "運動用品" | "食品飲料";
-  price: number;
-  stock: number;
-  brand: string;
-  status: "" | "上架中" | "下架" | "缺貨中";
-  createdAt: string | null;
-  tags?: string;
-};
-
-type Products = Product[];
-
-type status = "idle" | "loading" | "succeeded" | "failed";
-
-type DataFormState = {
-  data: Products;
-  status: status;
-  error: string | null;
-  filtered: Products;
-  selected: Products;
-  del_data: Products;
-  newItem: Product;
-  keyword: string;
-  dateRange: {
-    start: string;
-    end: string;
-  };
-  selectAll: boolean;
-  filter: boolean; // 判定目前是否正在過濾資料
-  loading: boolean;
-  addPage: boolean; // 是否開啟新增Item頁面
-  delPage: boolean; // 是否開啟垃圾桶頁面
-  revisePage: {
-    // 是否開啟修改頁面
-    isOpen: boolean;
-    reviseItem: Product;
-  };
-  // 依照資料表格名稱做排列
-  props_sort_condition: {
-    No: boolean;
-    ID: boolean;
-    Name: boolean;
-    Brand: boolean;
-    Category: boolean;
-    Stock: boolean;
-  };
-  conditions: {
-    On_Sale: boolean; // 上架中
-    Off_Sale: boolean; // 下架
-    Out_of_Stock: boolean; // 缺貨
-  };
-  cate_Condition: {
-    house: boolean;
-    stationery: boolean;
-    electronics: boolean;
-    sporting_goods: boolean;
-    food_and_beverage: boolean;
-  };
-  isVisible: {
-    ID: boolean;
-    Name: boolean;
-    Brand: boolean;
-    Category: boolean;
-    Price: boolean;
-    Date: boolean;
-    Status: boolean;
-    Stock: boolean;
-    Tags: boolean;
-  };
-};
 
 const initialState: DataFormState = {
   data: [],
@@ -513,7 +439,7 @@ const dataFormSlice = createSlice({
       // name === "tags" ? value.split(",").map((i) => i.trim()) : value
       state.newItem = {
         ...state.newItem,
-        [name]: name === "tags" ? value.split(",").map((i) => i.trim()) : value,
+        [name]: name === "tags" ? value.split(",").map((i :string) => i.trim()) : value,
       };
     },
     addData(state) {
