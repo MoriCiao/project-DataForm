fb6d7c11c2bc12bb14290d7a3c373f0110dee8da
MoriCiao <bboynoice@gmail.com>
2025-09-30 14:36:20 +0800

    refactor: 將Header 圖片根據裝置大小切換

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,13 +1,22 @@
 const STYLE = {
-  header: `header flex h-[25vh] w-full items-center justify-center overflow-hidden pr-24 select-none sm:pt-8 md:pt-12 lg:h-[30vh] xl:pt-8 xl:pb-2`,
+  header: `header flex h-[25vh] items-center justify-center overflow-hidden pr-12 select-none sm:pt-8 md:pt-12 lg:h-[30vh] lg:pr-24 xl:pt-8 xl:pb-2`,
 };
+
+const imgSrc = {
+  small: "/project-DataForm/DataForm-small.webp",
+  medium: "/project-DataForm/DataForm-medium.webp",
+  large: "/project-DataForm/DataForm-large.webp",
+};
+
+const { small, medium, large } = imgSrc;
 
 const Header = () => {
   return (
     <header className={STYLE.header}>
       <img
         className="object-cover md:w-[80%] xl:w-[50%]"
-        src="/project-DataForm/Title-DataForm_header.webp"
+        srcset={`${small} 480w, ${medium} 768w, ${large} 1280w`}
+        sizes="(max-width: 768px) 100vw, (max-width: 1280px) 80vw"
         alt="Header圖片"
         loading="lazy"
         draggable="false"
